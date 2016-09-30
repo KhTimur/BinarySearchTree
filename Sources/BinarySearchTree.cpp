3984bb4f5ad08e28e812f9168e16e3e536597403
KhTimur <khazirov@yandex.ru>
2016-09-30 17:29:11 +0400

    Add files via upload

---
+++ Sources/BinarySearchTree.cpp
@@ -0,0 +1,15 @@
+#include "BinarySearchTree.h"
+
+BinarySearchTree::BinarySearchTree(const std::initializer_list <T> &list)
+{
+    for (auto& value : list)
+        insert(value);
+}
+
+BinarySearchTree::~BinarySearchTree()
+{
+    delete root_;
+    size_ = 0;
+}
+
+BinarySearchTree::
